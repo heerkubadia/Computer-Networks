6d55d2826c8577feec8298c0845e790eb3048314
Heer Kubadia <117744600+heerkubadia@users.noreply.github.com>
2025-04-13 00:20:53 +0530

    Update topology.py

--- question2/topology.py
+++ question2/topology.py
@@ -7,25 +7,22 @@ from mininet.log import setLogLevel
 import time
 import os
 
-# Clean up any previous Mininet setup
-os.system('mn -c')
-
 class LoopTopo(Topo):
     def build(self):
-        # Add switches
+        # Add switches with STP enabled
         s1 = self.addSwitch('s1', stp=True)
         s2 = self.addSwitch('s2', stp=True)
         s3 = self.addSwitch('s3', stp=True)
         s4 = self.addSwitch('s4', stp=True)
 
-        # Add NAT gateway host H9
-        h9 = self.addHost('h9')  # NAT router without initial IP
-        
-        # Add H1 and H2, connected to H9 instead of s1
+        # Add NAT gateway host
+        h9 = self.addHost('h9')  # Acts as NAT router
+
+        # Add internal hosts connected to NAT
         h1 = self.addHost('h1', ip='10.1.1.2/24')
         h2 = self.addHost('h2', ip='10.1.1.3/24')
         
-        # Add other hosts
+        # Add external hosts
         h3 = self.addHost('h3', ip='10.0.0.4/24')
         h4 = self.addHost('h4', ip='10.0.0.5/24')
         h5 = self.addHost('h5', ip='10.0.0.6/24')
@@ -33,14 +30,14 @@ class LoopTopo(Topo):
         h7 = self.addHost('h7', ip='10.0.0.8/24')
         h8 = self.addHost('h8', ip='10.0.0.9/24')
 
-        # Internal links to NAT (h9)
+        # Connect internal hosts to NAT
         self.addLink(h1, h9, cls=TCLink, delay='5ms')
         self.addLink(h2, h9, cls=TCLink, delay='5ms')
-        
-        # External connectivity
-        self.addLink(h9, s1, cls=TCLink, delay='5ms')  # h9 external side
-        
-        # Link remaining hosts to respective switches with 5ms delay
+
+        # Connect NAT to switch s1 for external access
+        self.addLink(h9, s1, cls=TCLink, delay='5ms')
+
+        # Connect external hosts to switches
         self.addLink(h3, s2, cls=TCLink, delay='5ms')
         self.addLink(h4, s2, cls=TCLink, delay='5ms')
         self.addLink(h5, s3, cls=TCLink, delay='5ms')
@@ -48,7 +45,7 @@ class LoopTopo(Topo):
         self.addLink(h7, s4, cls=TCLink, delay='5ms')
         self.addLink(h8, s4, cls=TCLink, delay='5ms')
 
-        # Add switch-switch links with 7ms latency
+        # Add inter-switch links to form a loop topology
         self.addLink(s1, s2, cls=TCLink, delay='7ms')
         self.addLink(s2, s3, cls=TCLink, delay='7ms')
         self.addLink(s3, s4, cls=TCLink, delay='7ms')
@@ -60,61 +57,56 @@ def run():
     net = Mininet(topo=topo, switch=OVSSwitch, controller=OVSController, autoSetMacs=True)
     net.start()
     
-    # Get host and switch objects
+    # Retrieve hosts and switches
     h1, h2, h3, h4, h5, h6, h7, h8, h9 = net.get('h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9')
     
     # Enable STP on all switches
     for sw in ['s1', 's2', 's3', 's4']:
-        sw_obj = net.get(sw)
-        sw_obj.cmd('ovs-vsctl set Bridge {} stp_enable=true'.format(sw))
+        net.get(sw).cmd(f'ovs-vsctl set Bridge {sw} stp_enable=true')
     
-    # Clear any existing IPs on h9
+    # Clear any existing IPs on NAT interfaces
     h9.cmd("ip addr flush dev h9-eth0")
     h9.cmd("ip addr flush dev h9-eth1")
     h9.cmd("ip addr flush dev h9-eth2")
     
-    # Create a bridge on h9 to join internal interfaces
+    # Create bridge on NAT for internal connections
     h9.cmd('ip link add name br0 type bridge')
     h9.cmd('ip link set br0 up')
-    h9.cmd('ip link set h9-eth0 master br0')  # link to h1
-    h9.cmd('ip link set h9-eth1 master br0')  # link to h2
-    h9.cmd('ip addr add 10.1.1.1/24 dev br0')  # internal gateway IP
+    h9.cmd('ip link set h9-eth0 master br0')  # Connect to h1
+    h9.cmd('ip link set h9-eth1 master br0')  # Connect to h2
+    h9.cmd('ip addr add 10.1.1.1/24 dev br0')  # Internal gateway IP
     
-    # Set external IP
+    # Assign external IP to NAT
     h9.setIP('10.0.0.1/24', intf='h9-eth2')
     h9.cmd('ip addr add 172.16.10.10/24 dev h9-eth2')
-    
-    # Set default routes for internal hosts
+
+    # Configure default gateways for internal hosts
     h1.cmd('ip route add default via 10.1.1.1')
     h2.cmd('ip route add default via 10.1.1.1')
-    
-    # Set default route on external hosts
-    h3.cmd('ip route add default via 10.0.0.1')
-    h4.cmd('ip route add default via 10.0.0.1')
-    h5.cmd('ip route add default via 10.0.0.1')
-    h6.cmd('ip route add default via 10.0.0.1')
-    h7.cmd('ip route add default via 10.0.0.1')
-    h8.cmd('ip route add default via 10.0.0.1')
+
+    # Configure default gateways for external hosts
+    for h in [h3, h4, h5, h6, h7, h8]:
+        h.cmd('ip route add default via 10.0.0.1')
     
     # Enable IP forwarding on NAT
     h9.cmd('sysctl -w net.ipv4.ip_forward=1')
     
-    # Setup NAT: masquerade internal traffic via h9-eth2
+    # Set up NAT rules for internal to external masquerading
     h9.cmd('iptables -t nat -F')
     h9.cmd('iptables -t nat -A POSTROUTING -s 10.1.1.0/24 -o h9-eth2 -j MASQUERADE')
     
-    print("Waiting 30 seconds for routes/NAT to settle...")
+    print("Waiting 30 seconds for network to stabilize...")
     time.sleep(30)
     
-    # Test connectivity
-    print("\nTesting basic connectivity with pingAll:")
+    # Test network connectivity
+    print("\nTesting connectivity across the network:")
     net.pingAll()
     
-    print("\nNetwork started with NAT configured. Use CLI to interact.")
+    print("\nNAT network ready. Use CLI to interact with hosts.")
     CLI(net)
     
     net.stop()
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     setLogLevel('info')
     run()
